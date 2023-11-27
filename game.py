3e87299f785e57bbe18fe482e27c42023ec4ef91
mbarela82 <mbarela82@gmail.com>
2023-11-27 09:34:20 -0700

    first commit

---
+++ game.py
@@ -0,0 +1,45 @@
+import sys
+
+import pygame
+
+from scripts.entities import PhysicsEntity
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("ninja game")
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False]
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
